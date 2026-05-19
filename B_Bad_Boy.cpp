33e4f739c13323b0225d40ce0e3fed449d50de59
Bishal Baira <bishalbaira432@gmail.com>
2026-05-19 18:13:46 +0530

    B_Bad_Boy

---
+++ B_Bad_Boy.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sol()
+{
+    int n, m, i, j;
+    cin >> n >> m >> i >> j;
+
+    cout << 1 << " " << 1 << " " << n << " " << m << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int testCaseInp;
+    cin >> testCaseInp;
+
+    while (testCaseInp--)
+    {
+        sol();
+    }
+
+    return 0;
+}
